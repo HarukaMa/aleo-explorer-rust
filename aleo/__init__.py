dec07f2f5eac3b44c3e7aa08dec63eb8aba99d53
Haruka <mrx@hcc.im>
2023-04-20 19:33:07 +0900

    use maturin

--- aleo/__init__.py
+++
@@ -1,9 +0,0 @@
-# import the contents of the Rust library into the Python extension
-# optional: include the documentation from the Rust module
-from .aleo import *
-
-__all__ = [
-    "sign_nonce",
-    "bech32_encode",
-    "bech32_decode",
-]
--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-from setuptools_rust import Binding, RustExtension
-
-setup(
-    rust_extensions=[RustExtension("aleo.aleo", binding=Binding.PyO3)],
-)
