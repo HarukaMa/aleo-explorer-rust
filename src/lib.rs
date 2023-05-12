98031b540ae3e3052779373c34303470a52a6db6
Haruka <mrx@hcc.im>
2023-05-12 20:59:10 +0900

    add mapping ids

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::str::FromStr;
-use bech32::{FromBase32, ToBase32, Variant};
 
+use bech32::{FromBase32, ToBase32, Variant};
 use pyo3::exceptions;
 use pyo3::prelude::*;
-use snarkvm::prelude::{PrivateKey, Signature, Testnet3, ToBytes};
+use snarkvm::prelude::traits::ToBits;
+use snarkvm::prelude::{Field, FromBytes, Identifier, Network, PrivateKey, ProgramID, Signature, Testnet3, ToBytes};
+use snarkvm_console_program::Plaintext;
 
 #[pymodule]
 #[pyo3(name = "aleo")]
@@ -11,6 +13,9 @@ fn module(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sign_nonce, m)?)?;
     m.add_function(wrap_pyfunction!(bech32_decode, m)?)?;
     m.add_function(wrap_pyfunction!(bech32_encode, m)?)?;
+    m.add_function(wrap_pyfunction!(get_mapping_id, m)?)?;
+    m.add_function(wrap_pyfunction!(get_key_id, m)?)?;
+    m.add_function(wrap_pyfunction!(get_value_id, m)?)?;
     Ok(())
 }
 
@@ -29,21 +34,50 @@ fn sign_nonce(private_key: &str, nonce: &[u8]) -> PyResult<Vec<u8>> {
 
 #[pyfunction]
 fn bech32_encode(hrp: &str, bytes: &[u8]) -> PyResult<String> {
-    bech32::encode(hrp, bytes.to_base32(), Variant::Bech32m).map_err(|err| {
-        exceptions::PyValueError::new_err(format!(
-            "unable to encode bech32: {}",
-            err.to_string()
-        ))
-    })
+    bech32::encode(hrp, bytes.to_base32(), Variant::Bech32m)
+        .map_err(|err| exceptions::PyValueError::new_err(format!("unable to encode bech32: {}", err.to_string())))
 }
 
 #[pyfunction]
 fn bech32_decode(data: &str) -> PyResult<(String, Vec<u8>)> {
-    let (hrp, data, _) = bech32::decode(data).map_err(|err| {
-        exceptions::PyValueError::new_err(format!(
-            "unable to decode bech32: {}",
-            err.to_string()
-        ))
-    })?;
+    let (hrp, data, _) = bech32::decode(data)
+        .map_err(|err| exceptions::PyValueError::new_err(format!("unable to decode bech32: {}", err.to_string())))?;
     Ok((hrp, Vec::<u8>::from_base32(&data).unwrap()))
+}
+
+#[pyfunction]
+fn get_mapping_id(program_id: &str, mapping_name: &str) -> PyResult<String> {
+    let program_id = ProgramID::<Testnet3>::from_str(program_id)
+        .map_err(|_| exceptions::PyValueError::new_err("invalid program id"))?;
+    let mapping_name = Identifier::<Testnet3>::from_str(mapping_name)
+        .map_err(|_| exceptions::PyValueError::new_err("invalid mapping name"))?;
+    <Testnet3 as Network>::hash_bhp1024(&(program_id, mapping_name).to_bits_le())
+        .map(|hash| hash.to_string())
+        .map_err(|_| exceptions::PyValueError::new_err("invalid mapping id"))
+}
+
+#[pyfunction]
+fn get_key_id(mapping_id: &str, key: &[u8]) -> PyResult<String> {
+    let mapping_id =
+        Field::<Testnet3>::from_str(mapping_id).map_err(|_| exceptions::PyValueError::new_err("invalid mapping id"))?;
+    let key =
+        Plaintext::<Testnet3>::from_bytes_le(key).map_err(|_| exceptions::PyValueError::new_err("invalid key"))?;
+    let key_hash = <Testnet3 as Network>::hash_bhp1024(&key.to_bits_le())
+        .map_err(|_| exceptions::PyValueError::new_err("invalid key"))?;
+    <Testnet3 as Network>::hash_bhp1024(&(mapping_id, key_hash).to_bits_le())
+        .map(|hash| hash.to_string())
+        .map_err(|_| exceptions::PyValueError::new_err("invalid key id"))
+}
+
+#[pyfunction]
+fn get_value_id(key_id: &str, value: &[u8]) -> PyResult<String> {
+    let key_id =
+        Field::<Testnet3>::from_str(key_id).map_err(|_| exceptions::PyValueError::new_err("invalid key id"))?;
+    let value =
+        Plaintext::<Testnet3>::from_bytes_le(value).map_err(|_| exceptions::PyValueError::new_err("invalid value"))?;
+    let value_hash = <Testnet3 as Network>::hash_bhp1024(&value.to_bits_le())
+        .map_err(|_| exceptions::PyValueError::new_err("invalid value"))?;
+    <Testnet3 as Network>::hash_bhp1024(&(key_id, value_hash).to_bits_le())
+        .map(|hash| hash.to_string())
+        .map_err(|_| exceptions::PyValueError::new_err("invalid value id"))
 }
