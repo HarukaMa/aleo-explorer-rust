07dfea2d28c1c0a18c37661c0115f6df8bf032c9
Haruka Ma <mrx@hcc.im>
2023-05-15 14:51:25 +0900

    shorthand network type

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ use snarkvm::prelude::traits::ToBits;
 use snarkvm::prelude::{Field, FromBytes, Identifier, Network, PrivateKey, ProgramID, Signature, Testnet3, ToBytes};
 use snarkvm_console_program::{Plaintext, Value};
 
+type N = Testnet3;
+
 #[pymodule]
 #[pyo3(name = "aleo")]
 fn module(_py: Python, m: &PyModule) -> PyResult<()> {
@@ -21,7 +23,7 @@ fn module(_py: Python, m: &PyModule) -> PyResult<()> {
 
 #[pyfunction]
 fn sign_nonce(private_key: &str, nonce: &[u8]) -> PyResult<Vec<u8>> {
-    let private_key = PrivateKey::<Testnet3>::from_str(private_key)
+    let private_key = PrivateKey::<N>::from_str(private_key)
         .map_err(|_| exceptions::PyValueError::new_err("invalid private key"))?;
     Signature::sign_bytes(&private_key, nonce, &mut rand::thread_rng())
         .map(|signature| {
@@ -47,11 +49,11 @@ fn bech32_decode(data: &str) -> PyResult<(String, Vec<u8>)> {
 
 #[pyfunction]
 fn get_mapping_id(program_id: &str, mapping_name: &str) -> PyResult<String> {
-    let program_id = ProgramID::<Testnet3>::from_str(program_id)
+    let program_id = ProgramID::<N>::from_str(program_id)
         .map_err(|_| exceptions::PyValueError::new_err("invalid program id"))?;
-    let mapping_name = Identifier::<Testnet3>::from_str(mapping_name)
+    let mapping_name = Identifier::<N>::from_str(mapping_name)
         .map_err(|_| exceptions::PyValueError::new_err("invalid mapping name"))?;
-    <Testnet3 as Network>::hash_bhp1024(&(program_id, mapping_name).to_bits_le())
+    <N as Network>::hash_bhp1024(&(program_id, mapping_name).to_bits_le())
         .map(|hash| hash.to_string())
         .map_err(|_| exceptions::PyValueError::new_err("invalid mapping id"))
 }
@@ -59,12 +61,12 @@ fn get_mapping_id(program_id: &str, mapping_name: &str) -> PyResult<String> {
 #[pyfunction]
 fn get_key_id(mapping_id: &str, key: &[u8]) -> PyResult<String> {
     let mapping_id =
-        Field::<Testnet3>::from_str(mapping_id).map_err(|_| exceptions::PyValueError::new_err("invalid mapping id"))?;
+        Field::<N>::from_str(mapping_id).map_err(|_| exceptions::PyValueError::new_err("invalid mapping id"))?;
     let key =
-        Plaintext::<Testnet3>::from_bytes_le(key).map_err(|_| exceptions::PyValueError::new_err("invalid key"))?;
-    let key_hash = <Testnet3 as Network>::hash_bhp1024(&key.to_bits_le())
+        Plaintext::<N>::from_bytes_le(key).map_err(|_| exceptions::PyValueError::new_err("invalid key"))?;
+    let key_hash = <N as Network>::hash_bhp1024(&key.to_bits_le())
         .map_err(|_| exceptions::PyValueError::new_err("invalid key"))?;
-    <Testnet3 as Network>::hash_bhp1024(&(mapping_id, key_hash).to_bits_le())
+    <N as Network>::hash_bhp1024(&(mapping_id, key_hash).to_bits_le())
         .map(|hash| hash.to_string())
         .map_err(|_| exceptions::PyValueError::new_err("invalid key id"))
 }
@@ -72,12 +74,12 @@ fn get_key_id(mapping_id: &str, key: &[u8]) -> PyResult<String> {
 #[pyfunction]
 fn get_value_id(key_id: &str, value: &[u8]) -> PyResult<String> {
     let key_id =
-        Field::<Testnet3>::from_str(key_id).map_err(|_| exceptions::PyValueError::new_err("invalid key id"))?;
+        Field::<N>::from_str(key_id).map_err(|_| exceptions::PyValueError::new_err("invalid key id"))?;
     let value =
-        Value::<Testnet3>::from_bytes_le(value).map_err(|_| exceptions::PyValueError::new_err("invalid value"))?;
-    let value_hash = <Testnet3 as Network>::hash_bhp1024(&value.to_bits_le())
+        Value::<N>::from_bytes_le(value).map_err(|_| exceptions::PyValueError::new_err("invalid value"))?;
+    let value_hash = <N as Network>::hash_bhp1024(&value.to_bits_le())
         .map_err(|_| exceptions::PyValueError::new_err("invalid value"))?;
-    <Testnet3 as Network>::hash_bhp1024(&(key_id, value_hash).to_bits_le())
+    <N as Network>::hash_bhp1024(&(key_id, value_hash).to_bits_le())
         .map(|hash| hash.to_string())
         .map_err(|_| exceptions::PyValueError::new_err("invalid value id"))
 }
